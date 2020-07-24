b1214a0205def2fa008b1879bb04bf7e9b9d90e0
zhaojun98 <13698274091@163.com>
2020-07-24 17:56:45 +0800

    配置文件修改

--- src/main/java/com/bdqn/ErpApplication.java
+++ src/main/java/com/bdqn/ErpApplication.java
@@ -2,11 +2,13 @@ package com.bdqn;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 //加载mapper接口所在的目录
 @MapperScan("com.bdqn.*.dao")
+@SpringBootApplication
 public class ErpApplication {
 
     public static void main(String[] args) {
